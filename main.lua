fb3a12166b47eb1839cf6927cdc8565e525c215d
lonlydoge <82283198+lonlydoge@users.noreply.github.com>
2021-10-15 18:38:27 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+loadstring(getgenv().Code)()
+for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
+	if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
+		game:GetService("RunService").Heartbeat:connect(function()
+			v.Velocity = Vector3.new(-30,0,0)
+		end)
+	end
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "Netless";
+    Text = "Netless Applied To Script";
+    Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 16;
